49b87c79a811a69e8cb5435de92d12deef822dae
Taga Kouhei <tagahei0505@gmail.com>
2023-03-13 15:39:01 +0900

    コメント付け等少し整理

--- src/ARReader.js
+++ src/ARReader.js
@@ -1,108 +1,90 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from "react";
 // firebase
 import firebase from "./firebase";
-import { collection, addDoc, doc, updateDoc ,getDoc} from "firebase/firestore";
-import { ref, getStorage, getDownloadURL  } from "firebase/storage";
+import { doc, getDoc } from "firebase/firestore";
+import { ref, getStorage, getDownloadURL } from "firebase/storage";
 
-const ARComponent = () => {  
+const ARComponent = () => {
+  const [loading, setLoading] = useState(false);
+
   // id取得
-  let url = new URL(window.location.href);
-  let params = url.searchParams;
-  console.log(params.get('user_id')); // 5
+  // let url = new URL(window.location.href);
+  // let params = url.searchParams;
+  // var user_id = params.get("user_id");
+  var user_id = "TzxJ9ox39PmW84TgS19x";
+  console.log("uid取得:" + user_id);
 
-  // // IDの取得 おそらくGET
-  var user_id = params.get('user_id');
-  // var user_id = "zC1oHhStWJvEIQrBC1fT";
+  // 動画path（デフォルトを用意）
+  const [moviefile_path, setMoviePath] = useState(
+    process.env.PUBLIC_URL + "/testsrc/Debug.mp4"
+  );
+  // パターンファイルpath
+  const [markerpatternfile_path, setPattaernPath] = useState(
+    process.env.PUBLIC_URL + "/testsrc/pattern-Debugmarker.patt"
+  );
 
-
-  // 動画path
-  const [moviefile_path, setMoviePath] = useState(process.env.PUBLIC_URL + "/testsrc/Debug.mp4");
-  const [markerpatternfile_path, setPattaernPath] = useState(process.env.PUBLIC_URL + "/testsrc/pattern-Debugmarker.patt");
-
-  // const moviefile_path = process.env.PUBLIC_URL + "/testsrc/Debug.mp4";
-  // const markerpatternfile_path = process.env.PUBLIC_URL + "/testsrc/pattern-Debugmarker.patt";
-
-  // setMoviePath()
-  // setPattaernPath()
-
-  const conectDB = async()=>{
+  const conectDB = async () => {
     // DBからpathの取得 非同期処理注意
     console.log("DB取得開始");
     const Doc = await getDoc(doc(firebase.db, "arbum_data", user_id));
-    console.log(typeof(Doc));
+    console.log(typeof Doc);
     console.log(Doc.data().marker_pattern_path);
     console.log(Doc.data().movie_path);
     const DB_marker_pattern_path = Doc.data().marker_pattern_path;
     const DB_movie_path = Doc.data().movie_path;
     console.log("DB取得完了");
-    setMoviePath(GetMovie(DB_movie_path))
-    setPattaernPath(Getpattern(DB_marker_pattern_path))
-  }
-  conectDB()
+    setMoviePath(GetMovie(DB_movie_path));
+    setPattaernPath(Getpattern(DB_marker_pattern_path));
 
+    // loading画面終了（この処理をDB取得完了時ではなく，a-frame作り終わったタイミングでしたい
+    setLoading(true);
+    console.log("loading完了");
+  };
+  
+  conectDB();
 
-  const GetMovie = (DB_movie_path) =>{
+  const GetMovie = (DB_movie_path) => {
     // pathからStorageにアクセス(今は直接アクセス)
     const storage = getStorage();
     const FirestoreRef_Movie = ref(storage, DB_movie_path);
-      getDownloadURL(FirestoreRef_Movie).then((url) => {
-      console.log("動画取得: " + url);
-      // setMoviePath(url)
-      console.log("動画取得完了");
-      return url;
-    })
-    .catch((error) => {
-      console.err("動画取得ERR: "+error);
-      // Handle any errors
-    });
-}
+    getDownloadURL(FirestoreRef_Movie)
+      .then((url) => {
+        console.log("動画取得: " + url);
+        // setMoviePath(url)
+        console.log("動画取得完了");
+        return url;
+      })
+      .catch((error) => {
+        console.err("動画取得ERR: " + error);
+        // Handle any errors
+      });
+  };
 
-const Getpattern = (DB_marker_pattern_path) => {
-      const storage = getStorage();
-  const FirestoreRef_Pattern = ref(storage, DB_marker_pattern_path);
-  getDownloadURL(FirestoreRef_Pattern).then((url) => {
-      console.log("patt取得開始" + url);  
-      // setPattaernPath(url);
-      console.log("patt取得完了" + url);  
-      return url;
-  })
-  .catch((error) => {
-    console.err("pattern取得ERR: "+error);
-  });
-}
+  const Getpattern = (DB_marker_pattern_path) => {
+    const storage = getStorage();
+    const FirestoreRef_Pattern = ref(storage, DB_marker_pattern_path);
+    getDownloadURL(FirestoreRef_Pattern)
+      .then((url) => {
+        console.log("patt取得開始" + url);
+        // setPattaernPath(url);
+        console.log("patt取得完了" + url);
+        return url;
+      })
+      .catch((error) => {
+        console.err("pattern取得ERR: " + error);
+      });
+  };
 
-  // conectDB();
+  const Viewloading=()=>{
+    return(
+      <p>Loadingテスト</p>
+    );
+  }
 
-
-
-
-  // pathからStorageにアクセス(今は直接アクセス)
-  // const storage = getStorage();
-  // const FirestoreRef_Movie = ref(storage, DB_movie_path);
-  //   getDownloadURL(FirestoreRef_Movie).then((url) => {
-  //   console.log("動画取得: " + url);
-  //   setMoviePath(url)
-  //   console.log("動画取得完了");
-  // })
-  // .catch((error) => {
-  //   console.err("動画取得ERR: "+error);
-  //   // Handle any errors
-  // });
-
-  // const FirestoreRef_Pattern = ref(storage, DB_marker_pattern_path);
-  // getDownloadURL(FirestoreRef_Pattern).then((url) => {
-  //     console.log("patt取得開始" + url);  
-  //     setPattaernPath(url);
-  //     console.log("patt取得完了" + url);  
-  // })
-  // .catch((error) => {
-  //   console.err("pattern取得ERR: "+error);
-  // });
-  
-
-  return (
-    <a-scene arjs="sourceWidth: window.innerWidth > window.innerHeight ? 640 : 480; sourceHeight: window.innerWidth > window.innerHeight ? 480 : 640">
-      <a-assets timeout="30000">
+  const ViewAR=()=>{
+    return (
+      <a-scene arjs="sourceWidth: window.innerWidth > window.innerHeight ? 640 : 480; sourceHeight: window.innerWidth > window.innerHeight ? 480 : 640">
+      <a-assets timeout="600000">
         <video
           autoPlay
           id="movie"
@@ -114,7 +96,7 @@ const Getpattern = (DB_marker_pattern_path) => {
       </a-assets>
 
       {/* <a-marker preset="hiro"> */}
-      <a-marker type="pattern" url={ markerpatternfile_path }>
+      <a-marker type="pattern" url={markerpatternfile_path}>
         <a-video
           src="#movie"
           width="4.6"
@@ -126,6 +108,15 @@ const Getpattern = (DB_marker_pattern_path) => {
       </a-marker>
       <a-entity camera></a-entity>
     </a-scene>
+  );
+  }
+
+  return (
+    <>
+      {!loading ? (Viewloading()):(ViewAR())}
+    </>
+    // ViewAR()
+    // Viewloading()
   );
 };
 
