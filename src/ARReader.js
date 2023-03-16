7d062d13bca2b9aa0fad1b599b4e60aa73008cc6
Taga Kouhei <tagahei0505@gmail.com>
2023-03-17 03:29:21 +0900

    ロード画面の解消

--- src/ARReader.js
+++ src/ARReader.js
@@ -139,15 +139,15 @@ const ARComponent = () => {
           <a-entity obj-model="obj: #tree-obj; mtl: #tree-mtl" model-loader></a-entity>
 
         <a-camera></a-camera>
-        <>
+        {/* <>
         {RemoveBack()}
-        </>
+        </> */}
       </a-scene>
     );
   };
 
   return (
-    <div className="ReaderBack" id="ReaderBack">
+    // <div className="ReaderBack" id="ReaderBack">
     <>
       {!loading ? DebugVIew_Loading() : ViewMoviefromMarker()}
       {/* {ViewMoviefromMarker()} */}
@@ -155,7 +155,7 @@ const ARComponent = () => {
       {/* {ViewMoviefromMarker()} */}
       {/* {DebugVIew_Loading()} */}
     </>
-    </div>
+    // </div>
 
   );
 };
